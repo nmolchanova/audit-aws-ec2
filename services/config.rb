d10f6d79d549d8cf3096b7dc7e5edc3c82814e34
Natalia <natalia@cloudcoreo.com>
2016-12-17 17:16:50 +0600

    CON-79: Check for unused security groups

--- services/config.rb
+++ services/config.rb
@@ -226,9 +226,57 @@ coreo_aws_advisor_alert "ec2-ports-range" do
   alert_when ["object[:to_port]"]
 end
 
+coreo_aws_advisor_alert "ec2-security-groups-list" do
+  action :define
+  service :ec2
+  display_name "Security Groups Inventory"
+  description "This rule lists all security groups."
+  category "Inventory"
+  suggested_action "None."
+  level "Informational"
+  objectives ["security_groups"]
+  audit_objects ["security_group_info.group_name"]
+  operators ["=~"]
+  alert_when [//]
+end
+
+coreo_aws_advisor_alert "ec2-instances-active-security-groups-list" do
+  action :define
+  service :ec2
+  display_name "EC2 Instances Security Groups Inventory"
+  description "This rule gets all active security groups for instances"
+  category "Inventory"
+  suggested_action "None."
+  level "Informational"
+  objectives ["instances"]
+  audit_objects ["reservation_set.instances_set.group_set.group_id"]
+  operators ["=~"]
+  alert_when [//]
+end
+
 coreo_aws_advisor_ec2 "advise-ec2" do
   action :advise
   alerts ${AUDIT_AWS_EC2_ALERT_LIST}
+  regions ${AUDIT_AWS_EC2_REGIONS}
+end
+
+coreo_aws_advisor_alert "elb-load-balancers-active-security-groups-list" do
+  action :define
+  service :elb
+  display_name "Elb load balancers active security groups list"
+  description "This rule gets all active security groups for load balancers"
+  category "Inventory"
+  suggested_action "None."
+  level "Informational"
+  objectives ["load_balancers"]
+  audit_objects ["load_balancer_descriptions.security_groups"]
+  operators ["=~"]
+  alert_when [//]
+end
+
+coreo_aws_advisor_elb "advise-elb" do
+  action :advise
+  alerts ${AUDIT_AWS_ELB_ALERT_LIST}
   regions ${AUDIT_AWS_EC2_REGIONS}
 end
 
@@ -252,6 +300,73 @@ coreo_uni_util_notify "advise-ec2-json" do
   endpoint ({
       :to => '${AUDIT_AWS_EC2_ALERT_RECIPIENT}', :subject => 'CloudCoreo ec2 advisor alerts on PLAN::stack_name :: PLAN::name'
   })
+end
+
+coreo_uni_util_jsrunner "security-groups" do
+  action :run
+  json_input '{
+      "ec2_report":COMPOSITE::coreo_aws_advisor_ec2.advise-ec2.report,
+      "elb_report":COMPOSITE::coreo_aws_advisor_elb.advise-elb.report,
+      "ec2_alerts_list":"${AUDIT_AWS_EC2_ALERT_LIST}",
+      "elb_alerts_list":"${AUDIT_AWS_ELB_ALERT_LIST}"
+  }'
+  function <<-EOH
+
+if( !json_input.ec2_alerts_list['ec2-security-groups-list'] ||
+    !json_input.ec2_alerts_list['ec2-instances-active-security-groups-list'] ||
+    !json_input.ec2_alerts_list['elb-load-balancers-active-security-groups-list'] ) {
+  callback(json_input.ec2_report);
+  return;
+}
+
+const activeSecurityGroups = [];
+
+const groupIsActive = (groupId) => {
+    for (let activeGroupId of activeSecurityGroups) {
+        if (activeGroupId === groupId) return true;
+    }
+    return false;
+};
+
+Object.keys(json_input.elb_report).forEach((key) => {
+    const violation = json_input.elb_report[key].violations['elb-load-balancers-active-security-groups-list'];
+    if (!violation) return;
+    violation.violating_object.forEach((obj) => {
+        obj.object.forEach((secGroup) => {
+            activeSecurityGroups.push(secGroup);
+        })
+    });
+});
+Object.keys(json_input.ec2_report).forEach((key) => {
+    const violation = json_input.ec2_report[key].violations['ec2-instances-active-security-groups-list'];
+    if (!violation) return;
+    violation.violating_object.forEach((obj) => {
+        activeSecurityGroups.push(obj.object.group_id);
+    });
+});
+
+Object.keys(json_input.ec2_report).forEach((key) => {
+    const tags = json_input.ec2_report[key].tags;
+    const violations = json_input.ec2_report[key].violations["ec2-security-groups-list"];
+    if (!violations) return;
+    violations.violating_object.forEach((item) => {
+        const currentSecGroup = item.object;
+        if (groupIsActive(currentSecGroup.group_id)) return;
+        const securityGroupIsNotUsedAlert = {
+            'display_name': 'EC2 security group is not used',
+            'description': 'Security group is not used anywhere',
+            'category': 'Audit',
+            'suggested_action': 'Remove this security group',
+            'level': 'Warning',
+            'region': violations.region
+        };
+        const violationKey = 'ec2-not-used-security-groups'
+        json_input.ec2_report[key].violations[violationKey] = securityGroupIsNotUsedAlert;
+    });
+});
+
+callback(json_input.ec2_report);
+  EOH
 end
 
 coreo_uni_util_jsrunner "tags-to-notifiers-array" do
@@ -338,127 +453,3 @@ COMPOSITE::coreo_uni_util_jsrunner.tags-rollup.return
   })
 end
 # END EC2
-
-
-coreo_aws_advisor_alert "get-security-groups" do
-  action :define
-  service :ec2
-  display_name "List of security groups"
-  description "Gets all security groups"
-  category "Inventory"
-  suggested_action "None."
-  level "Informational"
-  objectives ["security_groups"]
-  audit_objects ["security_group_info.group_name"]
-  operators ["=~"]
-  alert_when [//]
-end
-
-coreo_aws_advisor_alert "get-active-security-groups-for-instances" do
-  action :define
-  service :ec2
-  display_name "EC2 Instance Inventory"
-  description "This rule performs an inventory on all EC2 instances in the target AWS account."
-  category "Inventory"
-  suggested_action "None."
-  level "Information"
-  objectives ["instances"]
-  audit_objects ["reservation_set.instances_set.group_set.group_id"]
-  operators ["=~"]
-  alert_when [//]
-end
-
-coreo_aws_advisor_ec2 "advise-ec2" do
-  action :advise
-  alerts ["get-security-groups", "get-active-security-groups-for-instances"]
-  regions ${AUDIT_AWS_EC2_REGIONS}
-end
-
-coreo_aws_advisor_alert "get-active-security-groups-from-elb" do
-  action :define
-  service :elb
-  display_name "List of active security groups"
-  description "Gets all active security groups"
-  category "Inventory"
-  suggested_action "None."
-  level "Informational"
-  objectives ["load_balancers"]
-  audit_objects ["load_balancer_descriptions.security_groups"]
-  operators ["=~"]
-  alert_when [//]
-end
-
-coreo_aws_advisor_elb "advise-elb" do
-  action :advise
-  alerts ["get-active-security-groups-from-elb"]
-  regions ${AUDIT_AWS_EC2_REGIONS}
-end
-
-coreo_uni_util_jsrunner "security-groups" do
-  action :run
-  json_input '{
-      "security_groups_report":COMPOSITE::coreo_aws_advisor_ec2.advise-ec2.report,
-      "active_groups_report":COMPOSITE::coreo_aws_advisor_elb.advise-elb.report
-  }'
-  function <<-EOH
-const result = {};
-const activeSecurityGroups = [];
-const unusedSecGroups = [];
-
-const groupIsActive = (groupId) => {
-    for (let activeGroupId of activeSecurityGroups) {
-        if (activeGroupId === groupId) return true;
-    }
-    return false;
-};
-
-Object.keys(json_input.active_groups_report).forEach((key) => {
-    const violation = json_input.active_groups_report[key].violations['get-active-security-groups-from-elb'];
-    if (!violation) return;
-    violation.violating_object.forEach((obj) => {
-        obj.object.forEach((secGroup) => {
-            activeSecurityGroups.push(secGroup);
-        })
-    });
-});
-Object.keys(json_input.security_groups_report).forEach((key) => {
-    const violation = json_input.security_groups_report[key].violations['get-active-security-groups-for-instances'];
-    if (!violation) return;
-    violation.violating_object.forEach((obj) => {
-        activeSecurityGroups.push(obj.object.group_id);
-    });
-});
-
-console.log(activeSecurityGroups);
-
-Object.keys(json_input.security_groups_report).forEach((key) => {
-    const tags = json_input.security_groups_report[key].tags;
-    const violations = json_input.security_groups_report[key].violations["get-security-groups"];
-    if (!violations) return;
-    violations.violating_object.forEach((item) => {
-        const currectSecGroup = item.object;
-        if (groupIsActive(currectSecGroup.group_id)) return;
-
-        unusedSecGroups.push(currectSecGroup);
-        const notUsedSecurityGroupAlert = {
-            violations:
-            { 'not-used-sequrity-groups':
-            {
-                'display_name': 'Security group is not used',
-                'description': 'Security group is not used anywhere',
-                'category': 'Audit',
-                'suggested_action': 'Remove this security group',
-                'level': 'Warning',
-                'region': violations.region
-            }
-            },
-            tags: tags
-        };
-        result[key] = notUsedSecurityGroupAlert;
-    });
-});
-
-console.log(result);
-callback(result);
-  EOH
-end
