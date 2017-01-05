d1ef1306c1491c438412a4d9b34677fa75aacb7a
Natalia <natalia@cloudcoreo.com>
2017-01-05 14:21:49 +0600

    CON-79: added reset for advisor variable

--- services/config.rb
+++ services/config.rb
@@ -255,7 +255,7 @@ coreo_aws_advisor_alert "ec2-instances-active-security-groups-list" do
 end
 
 coreo_aws_advisor_alert "ec2-not-used-security-groups" do
-  action :nothing
+  action :define
   service :ec2
   display_name "EC2 security group is not used"
   description "Security group is not used anywhere"
@@ -266,7 +266,7 @@ end
 
 coreo_aws_advisor_ec2 "advise-ec2" do
   action :advise
-  alerts 'ec2-security-groups-list, ec2-instances-active-security-groups-list'
+  alerts 'ec2-security-groups-list, ec2-instances-active-security-groups-list,' + ${AUDIT_AWS_EC2_ALERT_LIST}
   regions ${AUDIT_AWS_EC2_REGIONS}
 end
 
