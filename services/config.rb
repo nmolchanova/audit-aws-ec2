fbecbf8db38d0b9066ef72092625b029768ecca4
Paul Allen <pauldallen@gmail.com>
2017-03-15 14:35:26 -0700

    fixing jsrunner-commons version

--- services/config.rb
+++ services/config.rb
@@ -463,7 +463,7 @@ coreo_uni_util_jsrunner "ec2-tags-to-notifiers-array" do
   packages([
                {
                    :name => "cloudcoreo-jsrunner-commons",
-                   :version => "latest"
+                   :version => "1.9.2"
                },
                {
                    :name => "js-yaml",
