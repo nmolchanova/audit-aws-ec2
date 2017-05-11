96a4cdc24251182bfa2cbea59d7ce3b883c76edc
colemai <colemai@tcd.ie>
2017-05-11 12:00:56 -0700

    CON-504 permanently removed flow-logs-inventory from runner

--- services/config.rb
+++ services/config.rb
@@ -411,9 +411,7 @@ coreo_aws_rule_runner "vpcs-flow-logs-inventory" do
   action (("${AUDIT_AWS_EC2_ALERT_LIST}".include?("ec2-vpc-flow-logs")) ? :run : :nothing)
   service :ec2
   regions ${AUDIT_AWS_EC2_REGIONS}
-# turning off flow-logs-inventory temporarily - there is an error
-  rules ["vpc-inventory", "flow-logs-inventory"]
-  # rules ["vpc-inventory"]
+  rules ["vpc-inventory"]
 end
 
 coreo_uni_util_variables "ec2-planwide" do
