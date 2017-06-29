6e7d7f2ad7febc3c53e611c46436fb282326f3ef
omurbek.jk@gmail.com <omurbek.jk@gmail.com>
2017-06-29 15:57:45 +0600

    updated jsrunner module to 1.9.7-beta30 version

--- services/config.rb
+++ services/config.rb
@@ -643,7 +643,7 @@ coreo_uni_util_jsrunner "ec2-tags-to-notifiers-array" do
   packages([
                {
                    :name => "cloudcoreo-jsrunner-commons",
-                   :version => "1.9.7-beta31"
+                   :version => "1.9.7-beta30"
                },
                {
                    :name => "js-yaml",
