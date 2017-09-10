04820bedbe7e76ae10aba6ede15db804020f19b9
Ian <colemai@tcd.ie>
2017-09-10 11:57:51 +0200

    CON-631 added num viols back to security jsr

--- services/config.rb
+++ services/config.rb
@@ -513,6 +513,7 @@ coreo_uni_util_jsrunner "security-groups-ec2" do
                }])
   json_input '{
       "main_report":COMPOSITE::coreo_aws_rule_runner.advise-ec2.report,
+      "number_violations":COMPOSITE::coreo_aws_rule_runner.advise-ec2.number_violations,
       "ec2_report":COMPOSITE::coreo_aws_rule_runner.advise-unused-security-groups-ec2.report
   }'
   function <<-EOH
