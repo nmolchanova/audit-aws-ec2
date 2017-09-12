135590d085dd4e2c207c63caddf5fa0defb4c68a
omurbek <omurbek.jk@gmail.com>
2017-09-12 16:43:19 +0600

    CON-629 fix for s3 malformed policy

--- services/config.rb
+++ services/config.rb
@@ -834,8 +834,8 @@ coreo_aws_s3_policy "cloudcoreo-audit-aws-ec2-policy" do
 ,
 "Action": "s3:*",
 "Resource": [
-"arn:aws:s3:::${AUDIT_AWS_EC2_S3_NOTIFICATION_BUCKET_NAME}/*",
-"arn:aws:s3:::${AUDIT_AWS_EC2_S3_NOTIFICATION_BUCKET_NAME}"
+"arn:aws:s3:::bucket-${AUDIT_AWS_EC2_S3_NOTIFICATION_BUCKET_NAME}/*",
+"arn:aws:s3:::bucket-${AUDIT_AWS_EC2_S3_NOTIFICATION_BUCKET_NAME}"
 ]
 }
 ]
